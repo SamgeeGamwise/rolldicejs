a74766e76b755e68fbbcf16ae92477776e1febdd
Samuel Krohn <SANIKR@outlook.com>
2022-05-29 09:04:18 -0600

    Dnd Update

--- README.md
+++ README.md
@@ -26,7 +26,17 @@ const Dice = require('rolldicejs').default;
 ## Docs
 
 ```
+Dice.roll(dnd: string) -> number
 Dice.roll(numOfSides: number) -> number
 Dice.roll(count: number, numOfSides: number) -> number
 Dice.roll(count: number, numOfSides: number, details: boolean) -> number[]
+```
+
+## Usage
+
+```
+Dice.roll("2d6") -> 5
+Dice.roll(20) -> 14
+Dice.roll(2, 6) -> 7
+Dice.roll(4, 6, true) -> [6, 2, 4, 2]
 ```
--- src/Dice.ts
+++ src/Dice.ts
@@ -1,9 +1,30 @@
 export default class Dice {
-    public static roll(max: number): number
-    public static roll(count: number, max: number): number
-    public static roll(count: number, max: number, details: boolean): number[]
-    public static roll(first: number, second?: number, details: boolean = false): number | number[] {
-        if (typeof second === 'undefined') {
+    public static roll(dnd: string): number
+    public static roll(numOfSides: number): number
+    public static roll(diceCount: number, numOfSides: number): number
+    public static roll(diceCount: number, numOfSides: number, details: boolean): number[]
+    public static roll(first: number | string, second?: number, details: boolean = false): number | number[] {
+        if (typeof first !== "number") {
+            const amounts: number[] = first.toLowerCase().split("d").map((stringVal: string) => Number(stringVal))
+
+            if (amounts.length === 2) {
+                if (details) {
+                    const rollList: number[] = []
+                    for (let i = 0; i < amounts[0]; i++) {
+                        rollList.push(Dice.random(1, amounts[1]))
+                    }
+                    return rollList
+                } else {
+                    let sum: number = 0
+                    for (let i = 0; i < amounts[0]; i++) {
+                        sum += Dice.random(1, amounts[1])
+                    }
+                    return sum
+                }
+            } else {
+                return 0
+            }
+        } else if (typeof second === 'undefined') {
             return Dice.random(1, first)
         } else if (details) {
             const rollList: number[] = []
